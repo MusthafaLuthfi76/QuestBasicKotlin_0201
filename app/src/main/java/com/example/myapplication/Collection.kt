0a1a2b50d06df74b5c5ca4b4a9f921b60faa56bb
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-11 22:24:12 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Collection.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+fun ContohList() {
+    println("=== List ===")
+    // List Read Only
+    val readOnlyAbjad = listOf("A", "B", "C")
+    println(readOnlyAbjad)
+
+    // List Mutable
+    val shape: MutableList<String> = mutableListOf("Circle", "Square", "Triangle")
+    println(shape)
+
+    // Menambahkan data ke dalam list mutable
+    shape.add("Circle")
+    println(shape)
+
+    // Menghapus data dari List Mutable
+    shape.remove("Triangle")
+    println(shape)
+
+    // Mengubah data di dalam List Mutable
+    shape[0] = "Oval"
+    println(shape)
+
+    // List Read-Only
+    val shapesLocked: List<String> = shape
+    println(shapesLocked)
+}
