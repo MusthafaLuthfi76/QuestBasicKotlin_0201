3d6d73782228d52fd9ab7bb07d404d50ab1a0c3f
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-13 13:49:02 +0700

    Penjelasan Conditional Expression

---
+++ app/src/main/java/com/example/myapplication/ConditionalExpression.kt
@@ -0,0 +1,6 @@
+package com.example.myapplication
+
+// Conditional Expression
+// Conditional Expression adalah ekspresi yang mengembalikan nilai berdasarkan kondisi tertentu
+// Conditional expression menggunakan if-else, when, dan try-catch
+
