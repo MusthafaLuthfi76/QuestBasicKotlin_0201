26ab05fa47d7fcb7be4544ab1f6987dfdf784cf5
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-13 15:21:00 +0700

    Penjelasan Lambda Function

---
+++ app/src/main/java/com/example/myapplication/LambdaFunction.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+// Lambda Functions
+// Lambda Functions adalah fungsi yang tidak memiliki nama
+// Lambda Functions biasanya digunakan untuk membuat fungsi yang sederhana
+// Lambda Functions menggunakan tanda panah (->) untuk memisahkan parameter dan body fungsi
+
