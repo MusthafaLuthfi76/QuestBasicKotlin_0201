e068a2cf9a5b4af1cacac50b2fa077005d75c93e
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-13 13:43:01 +0700

    Contoh Set, Contoh Map, dan function main pada collection.kt

--- app/src/main/java/com/example/myapplication/Collection.kt
+++ app/src/main/java/com/example/myapplication/Collection.kt
@@ -35,5 +35,50 @@ fun ContohSet() {
     val readOnlyAbjad = setOf("A", "B", "C")
     println(readOnlyAbjad)
 
+    // Set Mutable
+    val shape: MutableSet<String> = mutableSetOf("Circle", "Square", "Triangle")
+    println(shape)
 
+    // Menambahkan data ke dalam Set Mutable
+    shape.add("Rectangle")
+    println(shape)
+
+    // Set Read-Only
+    val shapesLocked: Set<String> = shape
+    println(shapesLocked)
+}
+
+fun ContohMap() {
+    println()
+    println("=== Map ===")
+
+    // Map Read Only
+    val readOnlyShape: Map<String, Int> = mapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println (readOnlyShape)
+
+    // Map Mutable
+    val shape: MutableMap<String, Int> = mutableMapOf("Circle" to 1, "Square" to 2, "Triangle" to 3)
+    println(shape)
+
+    // Menambahkan data ke dalam Map Mutable
+    shape["Rectangle"] = 4
+    println(shape)
+
+    // Menghapus data dari Map Mutable
+    shape.remove("Circle")
+    println(shape)
+
+    // Mengubah data di dalam Map Mutable
+    shape["Square"] = 5
+    println(shape)
+
+    // Map Read-Only
+    val shapesLocked: Map<String, Int> = shape
+    println(shapesLocked)
+}
+
+fun main() {
+    ContohList()
+    ContohSet()
+    ContohMap()
 }
