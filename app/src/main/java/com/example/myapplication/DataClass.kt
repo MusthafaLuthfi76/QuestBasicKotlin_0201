1309684dba684a05c2f05eebec64be22f45df10a
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-13 14:03:16 +0700

    Penjelasan data class

---
+++ app/src/main/java/com/example/myapplication/DataClass.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+// Data Class
+// Data Class adalah class yang digunakan untuk Menyimpan data
+// Data class menyediakan fungsi untuk meng-override fungsi equals(), hashCode(), dan toString().
+// Data Class juga menyediakan fungsi copy() untuk membuat salinan objek dengan beberapa perubahan.
+
