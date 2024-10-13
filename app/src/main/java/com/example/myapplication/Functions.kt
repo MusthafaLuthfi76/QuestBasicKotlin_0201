878536afa8fd5d67cadf0885d9d5c6bd4e1b5651
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-13 15:07:02 +0700

    Function with and without parameter

--- app/src/main/java/com/example/myapplication/Functions.kt
+++ app/src/main/java/com/example/myapplication/Functions.kt
@@ -1,2 +1,13 @@
 package com.example.myapplication
 
+fun withoutParamaeter(){
+    println("== withoutParameter ==")
+    println("Hello, World!")
+}
+
+fun withParameter(name: String) {
+    println()
+    println("== withParameter ==")
+    println("Hello, $name!")
+}
+
