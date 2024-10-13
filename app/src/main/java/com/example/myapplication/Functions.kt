eb5c69b8a1c3f051f97017b1abd317d26e61c920
MusthafaLuthfi76 <musthafa.luthfi76@gmail.com>
2024-10-13 15:19:01 +0700

    Function DefaultParameterValue

--- app/src/main/java/com/example/myapplication/Functions.kt
+++ app/src/main/java/com/example/myapplication/Functions.kt
@@ -19,3 +19,21 @@ fun withNamedArgument(name: String, age: Int){
     println("Hello, $name! You are $age years old.")
 }
 
+// Default parameter value
+// Default parameter value merupakan cara untuk memberikan nilai default pada parameter fungsi
+fun withDefaultParameter(name: String = " Indra", age: Int) {
+    println()
+    println("== withDefaultParameter ==")
+    println("Hello, $name! You are $age years old")
+}
+
+fun withReturn(panjang : Int, lebar : Int) {
+    return println(panjang * lebar)
+}
+
+fun main() {
+    withoutParamaeter()
+    withParameter("John")
+    withNamedArgument(name = "John", age = 30)
+    withDefaultParameter(age = 25)
+}
